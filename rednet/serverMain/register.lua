04e769b511f2c73b43b451748ba0557cc6704020
Appa <1pewdiepieisbetter1@gmail.com>
2026-06-08 01:36:27 -0400

    Register. Mainly for validating input from handler.lua

---
+++ rednet/serverMain/register.lua
@@ -0,0 +1,31 @@
+local Storage = require("storage")
+
+local function register(packet)
+    if type(packet.name) ~= "string"
+        or type(packet.serverId) ~= "number"
+        or type(packet.stargateAddress) ~= "string" then
+        return {
+            success = false,
+            error = "Malformed register packet. Received: "
+                .. tostring(packet.name) .. ", "
+                .. tostring(packet.serverId) .. ", "
+                .. tostring(packet.stargateAddress),
+            data = nil
+        }
+    end
+
+    return Storage.add({
+        name = packet.name,
+        serverId = packet.serverId,
+        stargateAddress = packet.stargateAddress
+    })
+end
+
+local function list()
+    return Storage.listNames() -- This follows same return format as seen above.
+end
+
+return {
+    register = register,
+    list = list,
+}
