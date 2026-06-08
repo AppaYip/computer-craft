8940cc632abd844a278fc57198a5ea127fb107d5
Appa <1pewdiepieisbetter1@gmail.com>
2026-06-08 17:57:23 -0400

    Moved

---
+++ stargate/registry/install.lua
@@ -0,0 +1,31 @@
+local base = "https://raw.githubusercontent.com/AppaYip/computer-craft/main/"
+
+local files = {
+    "stargate/registry/install.lua",
+    "stargate/registry/uninstall.lua",
+
+    "stargate/enums_actions.lua",
+    "stargate/enums_protocols.lua",
+
+    "stargate/registry/handler.lua",
+    "stargate/registry/register.lua",
+    "stargate/registry/server.lua",
+    "stargate/registry/storage.lua",
+
+    "helpers/require.lua",
+    "helpers/peripheral.lua",
+}
+
+print("Downloading files!")
+
+for _, file in ipairs(files) do
+    print("Downloading " .. file)
+    
+    local url = base .. file
+    local result = shell.run("wget", url, file)
+
+    if not result then
+        printError("Failed to install file: " .. file)
+    end
+end
+print("Install completed.")
