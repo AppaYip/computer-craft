993e8c112eef9d91acc15811edc71d2b6ccd8bb4
Appa <1pewdiepieisbetter1@gmail.com>
2026-06-08 02:14:59 -0400

    Install.lua
    This probably doesn't work

---
+++ rednet/registery/install.lua
@@ -0,0 +1,24 @@
+local base = "https://raw.githubusercontent.com/AppaYip/computer-craft/main/rednet/"
+
+local files = {
+    "enums_actions.lua",
+    "enums_protocols.lua",
+    "registery/handler.lua",
+    "registery/register.lua",
+    "registery/server.lua",
+    "registery/storage.lua"
+}
+
+print("Downloading files!")
+
+for _, file in ipairs(files) do
+    print("Downloading " .. file)
+    
+    local url = base .. file
+    local result = shell.run("wget", url, file)
+
+    if not result then
+        print("Failed to install file: "..file)
+    end
+end
+print("Install completed.")
