554c9d4c0359655ff1a14b30f11db8feefa84f08
Appa <1pewdiepieisbetter1@gmail.com>
2026-06-08 01:11:17 -0400

    Json storage for stargates

---
+++ rednet/serverMain/storage.lua
@@ -0,0 +1,75 @@
+local fileName = "stargates.json"
+
+local function loadDefault()
+    return { stargates = {} }
+end
+
+function getAll()
+    if not fs.exists(fileName) then
+        return loadDefault()
+    end
+
+    local file = fs.open(fileName, "r")
+    local content = file.readAll()
+    file.close()
+
+    local data = textutils.unserializeJSON(content)
+    if type(data) ~= "table" then
+        return loadDefault()
+    end
+
+    return  data
+end
+
+function save(data)
+    local file = fs.open(fileName, "w")
+    file.write(textutils.serializeJSON(data))
+    file.close()
+end
+
+function add(gatePacket)
+    local data = getAll()
+
+    table.insert(data.stargates, {
+        name = gatePacket.name,
+        serverId = gatePacket.serverId,
+        stargateAddress = gatePacket.stargateAddress
+    })
+
+    save(data)
+end
+
+local function listNames()
+    local data = getAll()
+    local list = {}
+
+    for _, gate in ipairs(data.stargates) do
+        table.insert(list, {
+            name = gate.name,
+            id= gate.serverId
+        })
+    end
+
+    return list
+end
+
+local function findByName(name)
+    local data = getAll()
+
+    for _, gate in ipairs(data.stargates) do
+        if gate.name == name then
+            return gate
+        end
+    end
+
+    return nil
+end
+
+
+return {
+    getAll = getAll,
+    save = save,
+    add = add,
+    listNames = listNames,
+    findByName = findByName
+}
