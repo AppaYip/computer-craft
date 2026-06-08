5e19ccfb4fdeb6ae0ba3b4d77e2cefa7a3c55884
Appa <1pewdiepieisbetter1@gmail.com>
2026-06-07 22:20:40 -0400

    Helper for finding peripherals

---
+++ helpers/peripheral.lua
@@ -0,0 +1,24 @@
+-- Attempts to find a peripheral With provided name. Returns true if success
+local function findOrWarn(type, filter)
+    filter = filter or function() return true end
+    local foundName
+    
+    local p = peripheral.find(type, function(name, p) 
+        if not filter or filter(name, p) then
+            foundName = name -- This is freaky but it works
+            return true
+        end
+    end)
+
+    if not p then
+        printError("Could not find peripheral of type: " .. type)
+        return nill
+    end
+
+    return {
+        name = foundName,
+        peripheral = p
+    }
+end
+
+return {findOrWarn = findOrWarn}
